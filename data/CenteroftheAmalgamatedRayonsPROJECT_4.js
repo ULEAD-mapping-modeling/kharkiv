2af0c10ec8f017038bf9f921a1539a8f1912a6d4
tduotg <tmokrasnogorivka@gmail.com>
2018-12-15 12:15:29 +0200

    COM1

---
+++ data/CenteroftheAmalgamatedRayonsPROJECT_4.js
@@ -0,0 +1,11 @@
+var json_CenteroftheAmalgamatedRayonsPROJECT_4 = {
+"type": "FeatureCollection",
+"name": "CenteroftheAmalgamatedRayonsPROJECT_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nameUA_new": "Харків" }, "geometry": { "type": "Point", "coordinates": [ 36.267385680375696, 49.976093806219197 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Ізюм" }, "geometry": { "type": "Point", "coordinates": [ 37.272932459068358, 49.181569608779398 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Куп'янськ" }, "geometry": { "type": "Point", "coordinates": [ 37.613391211175518, 49.71462766720002 ] } },
+{ "type": "Feature", "properties": { "nameUA_new": "Первомайський" }, "geometry": { "type": "Point", "coordinates": [ 36.212394320360758, 49.392873674028138 ] } }
+]
+}
